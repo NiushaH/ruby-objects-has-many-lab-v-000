b9c5e2a1d053e853163cea29610ed1516172c3ed
Niusha H <niusha.hadziomerovic@gmail.com>
2019-02-14 19:07:43 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,10 +1,15 @@
 class Post
   attr_accessor :title, :author
+
+@@all = []
  
   def initialize(title, author=nil)
     @title = title
     @author = author
   end
 
+  def new(title)
+    @@all << title
+  end  
  
 end
